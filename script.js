10e8f372c13ad7a7e700893c16d9ceb60486bf58
AidenSPS <aidenkusmierczak2016@gmail.com>
2022-04-06 08:23:46 -0400

    cancelButton works, working on passOk function

--- script.js
+++ script.js
@@ -1,11 +1,39 @@
 //Getting the buttons ID and declaring a variable
-let buttons = document.getElementById("buttons");
+var buttons = document.getElementById("buttons");
 
 //Ok button
-let oK = document.createElement("body");
-oK.setAttribute("id","ok");
-oK.setAttribute("onClick","passConfirm()");
+var oK = document.createElement("button");
+oK.setAttribute("id","OK");
+oK.setAttribute("onClick","passOk()");
 oK.innerText = "OK";
 buttons.appendChild(oK);
 
 //Cancel Button
+var cancel = document.createElement("button");
+cancel.setAttribute("id","cancel");
+cancel.setAttribute("onClick","winCan()");
+cancel.innerText = "Cancel";
+buttons.appendChild(cancel);
+
+//Function for Ok button
+function passOk(){
+	let userName = document.getElementById("username").value;
+	let passWord = document.getElementById("password").value;
+	let body = document.getElementsByClassName("body");
+	alert(userName);
+	alert(passWord);
+	if (userName == "Lawnmower Man"){
+		if(passWord = "7.3942"){
+			alert("Show Image Here");
+		}
+		else{
+			document.location= 'index.html';
+			body.setAttribute("background-color","red");
+		}
+	}
+}
+
+//Function for Cancel Button
+function winCan(){
+	document.location = 'index.html';
+}
